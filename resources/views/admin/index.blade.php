bfd186666ea89fc25ae4172142ce8d52815ff92e
plbryant <plbryant@hotmail.com>
2020-09-07 10:53:48 -0500

    template admin added

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,23 @@
+@extends('includes.backend')
+
+@section('content')
+<div class="row">
+  <div class="col-xl-12">
+    <div class="card">
+      <div class="card-header border-0">
+        <div class="row align-items-center">
+          <div class="col">
+            <h3 class="mb-0">Titulo</h3>
+          </div>
+          <!-- <div class="col text-right">
+                  <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                </div> -->
+        </div>
+      </div>
+      <div class="table-responsive">
+        <!-- Projects table -->
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
