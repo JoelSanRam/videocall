5f2ed24c1a5a6882f217ed8c2fce7ab99d684093
plbryant <plbryant@hotmail.com>
2020-09-08 14:27:52 -0500

    Agregando vistas sin personalizar

--- resources/views/admin/medico.blade.php
+++ resources/views/admin/medico.blade.php
@@ -7,7 +7,7 @@
       <div class="card-header border-0">
         <div class="row align-items-center">
           <div class="col">
-            <h3 class="mb-0">Principal</h3>
+            <h3 class="mb-0">Medico</h3>
           </div>
           <!-- <div class="col text-right">
                   <a href="#!" class="btn btn-sm btn-primary">See all</a>
