20bbe88db0c1a57640f7494be5b364f4885d0035
plbryant <plbryant@hotmail.com>
2020-09-08 15:06:52 -0500

    Rutas resource agregadas,falta anclarlas a vistas anteriormente realizadas

--- app/Http/Controllers/EnfermedadSintomaController.php
+++ app/Http/Controllers/EnfermedadSintomaController.php
@@ -2,83 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\EnfermedadSintoma;
 use Illuminate\Http\Request;
 
 class EnfermedadSintomaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\EnfermedadSintoma  $enfermedadSintoma
+   * @return \Illuminate\Http\Response
+   */
+  public function show(EnfermedadSintoma $enfermedadSintoma)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\EnfermedadSintoma  $enfermedadSintoma
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(EnfermedadSintoma $enfermedadSintoma)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\EnfermedadSintoma  $enfermedadSintoma
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, EnfermedadSintoma $enfermedadSintoma)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\EnfermedadSintoma  $enfermedadSintoma
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(EnfermedadSintoma $enfermedadSintoma)
+  {
+    //
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
 
+use App\Http\Controllers\EnfermedadSintomaController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//FrontEnd
 Route::get('/', function () {
   return view('front.index');
 });
 
+
+//Administrador
 Route::get('/administrar', function () {
   return view('admin.index');
 });
+
+Route::resource('/enfermedadSintoma', 'EnfermedadSintomaController')->names('enfermedad-sintoma');
+Route::resource('llamada', 'LlamadaController')->names('llamada');
+Route::resource('/medico', 'MedicoController')->names('medico');
+Route::resource('/pago', 'PagoController')->names('pago');
+Route::resource('/tipoUsuario', 'TipoUsuarioController')->names('tipo-usuario');
+Route::resource('/usuario', 'UsuarioController')->names('usuario');
